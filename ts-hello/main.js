2c0a8b3b4a48bf3e87bac7987a5b720faae27599
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-07-19 23:02:01 -0300

    Types

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,7 +1,23 @@
-function doSomething() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log('Finally: ' + i);
-}
-doSomething();
+var count = 5;
+// count = 'a';
+var a;
+// a = 1;
+// a = true;
+// a = 'a';
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, 'a', false];
+var ColorRed = 0;
+var ColerGreen = 1;
+var ColorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+    Color[Color["Purple"] = 3] = "Purple";
+})(Color || (Color = {}));
+;
+var backgroundColor = Color.Red;
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,9 +1,19 @@
-function doSomething() {
-  for (let i = 0; i < 5; i++) {
-    console.log(i);
-  }
+let count = 5;
+// count = 'a';
 
-  console.log('Finally: ' + i);
-}
+let a: number;
+// a = 1;
+// a = true;
+// a = 'a';
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, true, 'a', false];
 
-doSomething();
+const ColorRed = 0;
+const ColerGreen = 1;
+const ColorBlue = 2;
+
+enum Color { Red, Green, Blue, Purple };
+let backgroundColor = Color.Red;
