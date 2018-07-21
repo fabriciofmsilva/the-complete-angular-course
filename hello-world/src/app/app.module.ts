d1643c5c350089babe79748280efa3769b2b9f36
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-07-20 23:57:22 -0300

    Assignment 1: List of Authors

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -5,17 +5,21 @@ import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 import { CoursesComponent } from './courses.component';
 import { CoursesService } from './courses.service';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors/authors.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent
+    CourseComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [
+    AuthorsService,
     CoursesService
   ],
   bootstrap: [AppComponent]
