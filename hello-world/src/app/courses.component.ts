4a4057c3c15e13309a6791aeb2e54bfd3bc5195b
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-07-25 23:42:24 -0300

    Pipes

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -4,44 +4,19 @@ import { CoursesService } from './courses.service';
 @Component({
   selector: 'app-courses', // courses
   template: `
-    <h2>{{ title }}</h2>
-    <img [src]="imageUrl" />
-    <input [value]="email" (keyup.enter)="email = $event.target.value; onKeyUp()"/>
-    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()"/>
-    <button class="btn btn-primary" [class.active]="isActive" [style.backgroundColor]="isActive ? 'blue' : 'white'"
-      (click)="onSave($event)"
-    >Save</button>
-    <table>
-      <tr>
-        <td [attr.colspan]="colSpan"></td>
-      </tr>
-    </table>
-    <ul>
-      <li *ngFor="let course of courses">
-        {{ course }}
-      </li>
-    </ul>
+    {{ course.title | uppercase | lowercase }} <br/>
+    {{ course.students | number }} <br/>
+    {{ course.rating | number:'1.2-2' }} <br/>
+    {{ course.price | currency:'AUD':true:'3.2-2' }} <br/>
+    {{ course.releaseDate | date:'shortDate' }} <br/>
   `
 })
 export class CoursesComponent {
-  title = 'List of courses';
-  imageUrl = 'http://lorempixel.com/400/200';
-  colSpan = 2;
-  isActive = true;
-  courses;
-  email;
-
-  constructor(service: CoursesService) {
-    this.courses = service.getCourses();
-  }
-
-  onKeyUp() {
-    console.log(this.email);
-  }
-
-  onSave($event) {
-    $event.stopPropagation();
-
-    console.log('Button was clicked', $event);
-  }
+  course = {
+    title: 'The Complete Angular Course',
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2016, 3, 1)
+  };
 }
