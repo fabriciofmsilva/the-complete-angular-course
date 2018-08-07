cf1a39aaad6c7eb2311a92f9c091cce9ff514cb9
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-08-06 23:56:16 -0300

    Assignment 3: Title Casing

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AuthorsComponent } from './authors/authors.component';
 import { AuthorsService } from './authors/authors.service';
 import { SummaryPipe } from './summary.pipe';
 import { FavoriteComponent } from './favorite/favorite.component';
+import { TitleCasePipe } from './title-case.pipe';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FavoriteComponent } from './favorite/favorite.component';
     CourseComponent,
     AuthorsComponent,
     SummaryPipe,
-    FavoriteComponent
+    FavoriteComponent,
+    TitleCasePipe
   ],
   imports: [
     BrowserModule,
---
+++ hello-world/src/app/title-case.pipe.ts
@@ -0,0 +1,38 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'titleCase'
+})
+export class TitleCasePipe implements PipeTransform {
+
+  transform(value: string): any {
+    if (!value) {
+      return null;
+    }
+
+    const words = value.split(' ');
+    for (let i = 0; i < words.length; i++) {
+      if (i > 0 && this.isPreposition(words[i])) {
+        words[i] = words[i].toLowerCase();
+      } else {
+        words[i] = this.toTitleCase(words[i]);
+      }
+    }
+
+    return words.join(' ');
+  }
+
+  private isPreposition(word: string): boolean {
+    const prepositions = [
+      'of',
+      'the'
+    ];
+
+    return prepositions.includes(word.toLowerCase());
+  }
+
+  private toTitleCase(word: string): string {
+    return word.substr(0, 1).toUpperCase() + word.substr(1).toLowerCase();
+  }
+
+}
