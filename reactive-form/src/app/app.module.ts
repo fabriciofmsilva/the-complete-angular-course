cb767a81687747add76208e812b5e52e8c9802f3
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-08-13 23:07:20 -0300

    Building a Bootstrap Form

---
+++ reactive-form/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupFormComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
