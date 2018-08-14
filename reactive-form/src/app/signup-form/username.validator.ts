d9e96b53dd1fd2579c8d7cb7f2c3fbc5f8ef0a47
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-08-13 23:37:06 -0300

    Asynchronous Operations

--- reactive-form/src/app/signup-form/username.validator.ts
+++ reactive-form/src/app/signup-form/username.validator.ts
@@ -4,14 +4,18 @@ export class UsernameValidators {
   static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
     if ((control.value as string).indexOf(' ') >= 0) {
       return { cannotContainSpace: true };
-      // return {
-      //   minlength: {
-      //     requiredLength: 20,
-      //     actualLength: control.value.length
-      //   }
-      // };
     }
 
     return null;
+  }
+
+  static shouldBeUnique(control: AbstractControl): ValidationErrors | null {
+    setTimeout(() => {
+        if (control.value === 'mosh') {
+          return { shouldBeUnique: true };
+        }
+
+        return null;
+    }, 2000);
   }
 }
