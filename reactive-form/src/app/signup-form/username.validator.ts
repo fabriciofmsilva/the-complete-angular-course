3fae20af12f8becf948515cf20f1c19b2418f3b6
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-08-13 23:33:32 -0300

    Implementing Custom Validation

---
+++ reactive-form/src/app/signup-form/username.validator.ts
@@ -0,0 +1,17 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+export class UsernameValidators {
+  static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
+    if ((control.value as string).indexOf(' ') >= 0) {
+      return { cannotContainSpace: true };
+      // return {
+      //   minlength: {
+      //     requiredLength: 20,
+      //     actualLength: control.value.length
+      //   }
+      // };
+    }
+
+    return null;
+  }
+}
