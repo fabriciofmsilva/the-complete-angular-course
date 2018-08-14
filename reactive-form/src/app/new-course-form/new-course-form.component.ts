916b41855c081237c55b082fe13a28adce7fc9ba
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-08-14 00:11:48 -0300

    FormBuilder

--- reactive-form/src/app/new-course-form/new-course-form.component.ts
+++ reactive-form/src/app/new-course-form/new-course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormArray, FormControl } from '@angular/forms';
+import { FormGroup, FormArray, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-course-form',
@@ -8,8 +8,27 @@ import { FormGroup, FormArray, FormControl } from '@angular/forms';
 
 export class NewCourseFormComponent {
   form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    contact: new FormGroup({
+      email: new FormControl(),
+      phone: new FormControl()
+    }),
     topics: new FormArray([])
   });
+
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      name: [
+        '',
+        Validators.required
+      ],
+      contract: fb.group({
+        email: [],
+        phone: []
+      }),
+      topics: fb.array([])
+    });
+  }
 
   addTopic(topic: HTMLInputElement) {
     this.topics.push(new FormControl(topic.value));
