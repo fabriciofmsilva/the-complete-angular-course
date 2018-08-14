7abb8753865d4223d1d1e9780829843e2494e35f
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-08-14 00:07:58 -0300

    FormArray

--- reactive-form/src/app/app.module.ts
+++ reactive-form/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
-import { ReactiveFormsModule } from '../../node_modules/@angular/forms';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NewCourseFormComponent,
     SignupFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     ReactiveFormsModule
   ],
   providers: [],
